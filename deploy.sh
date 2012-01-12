3364774c93949343fcff1168a391179859385689
Etienne Kruger <etienne@acscomputers.co.za>
2012-01-12 20:20:32 +0200

    Updated deploy script to check for .ve first.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-virtualenv --no-site-packages .ve
+if [ ! -d ".ve" ]; then
+    echo "Creating virtualenv..."
+    virtualenv --no-site-packages .ve
+fi
 
 source ./.ve/bin/activate
 pip install -r ./requirements.txt
